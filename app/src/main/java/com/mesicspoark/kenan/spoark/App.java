6569df9703885e6dfbac242e7ddd18df3954e1fb
Kenan Mesic <kenmesi@yahoo.com>
2016-07-24 23:47:16 -0700

    Updated new parse sdk and deployed local parse server

--- app/src/main/java/com/mesicspoark/kenan/spoark/App.java
+++ app/src/main/java/com/mesicspoark/kenan/spoark/App.java
@@ -14,6 +14,12 @@ public class App extends Application {
          // Enable Local Datastore.
          Parse.enableLocalDatastore(this);
 
-         Parse.initialize(this, "whpfOJPCSABe6gyl9znlDSivZZfnOrnhLIPYxDvz", "6MCNggdLaTNf3P3fI9Z5hHaN2J0ZNb4of1hbwJ65");
+         Parse.initialize(new Parse.Configuration.Builder(this)
+                 .applicationId("spoark")
+                 .clientKey(null)
+                 .server("http://192.168.1.181:1337/parse/")
+                 .build()
+         );
+         //Parse.initialize(this, "whpfOJPCSABe6gyl9znlDSivZZfnOrnhLIPYxDvz", "6MCNggdLaTNf3P3fI9Z5hHaN2J0ZNb4of1hbwJ65");
      }
 }
